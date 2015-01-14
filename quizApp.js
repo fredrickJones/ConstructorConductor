bbb515d7fdeb966ee7432e890ea8c4c3d47d7165
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-13 17:43:01 -0700

    finished

--- quizApp.js
+++ quizApp.js
@@ -6,27 +6,42 @@
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+var User = function(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+};
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
+var Question = function(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+};
 
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
-
+var users = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
+users.push(new User('Bob', 'bob@gmail.com', 'bobDidIt'));
+users.push(new User('Rugga', 'rugga@gmail.com', 'ruggaDidIt'));
+users.push(new User('Sally', 'sally@gmail.com', 'sallyDidIt'));
 
 
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+var questions = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -35,6 +50,9 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
+questions.push(new Question('T/F:', 'Inheritance is achieved in JavaScript through Prototypes?', 'True'));
+questions.push(new Question('T/F:', 'JavaScript is just a scripting version of Java', 'False'));
+questions.push(new Question('T/F:', 'In Javascript, == doesn\'t check "type" but just the value - where === checks type and value', 'True'));
 
 
 //Now push all of your instances of Question into the questions Array
@@ -45,4 +63,13 @@ console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
   //code here
+for (var i = 0; i < users.length; i++) {
+	console.log(users[i]);
+};
+
+for (var i = 0; i < questions.length; i++) {
+	console.log(questions[i]);
+};
+
+
 
